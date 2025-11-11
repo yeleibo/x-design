1fcdf823508cb75913669223a7c129c7da17df85
Leibo Ye <sunshineboyye@gmail.com>
2025-11-11 18:34:28 +0800

    注释掉location定位

--- lib/src/services/location_service/location_service_implment_by_location.dart
+++ lib/src/services/location_service/location_service_implment_by_location.dart
@@ -1,46 +1,46 @@
-import 'package:location/location.dart';
-import 'package:x_design/xd_design.dart';
-
-
-///国外的推荐
-class LocationServiceImplementByLocation  with LocationBase implements LocationService{
-  XDLocation? _lastLocation;
-  bool _serviceEnabled=false;
-  PermissionStatus? _permissionGranted;
-  @override
-  Future<bool> init() async {
-    Location location =  Location();
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return false;
-      }
-    }
-
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return false;
-      }
-    }
-    location.onLocationChanged.listen((event) {
-      if(event.latitude!=null && event.longitude!=null){
-        _lastLocation=_transfer(event);
-        streamController.add(_lastLocation!);
-      }
-    });
-    return true;
-  }
-
-
-  XDLocation _transfer(LocationData locationData){
-    return XDLocation(latitude:locationData.latitude!,longitude: locationData.longitude!,altitude: locationData.altitude );
-  }
-
-  @override
-  XDLocation? getLastPosition() {
-    return _lastLocation;
-  }
-}
+// import 'package:location/location.dart';
+// import 'package:x_design/xd_design.dart';
+//
+//
+// ///国外的推荐
+// class LocationServiceImplementByLocation  with LocationBase implements LocationService{
+//   XDLocation? _lastLocation;
+//   bool _serviceEnabled=false;
+//   PermissionStatus? _permissionGranted;
+//   @override
+//   Future<bool> init() async {
+//     Location location =  Location();
+//     _serviceEnabled = await location.serviceEnabled();
+//     if (!_serviceEnabled) {
+//       _serviceEnabled = await location.requestService();
+//       if (!_serviceEnabled) {
+//         return false;
+//       }
+//     }
+//
+//     _permissionGranted = await location.hasPermission();
+//     if (_permissionGranted == PermissionStatus.denied) {
+//       _permissionGranted = await location.requestPermission();
+//       if (_permissionGranted != PermissionStatus.granted) {
+//         return false;
+//       }
+//     }
+//     location.onLocationChanged.listen((event) {
+//       if(event.latitude!=null && event.longitude!=null){
+//         _lastLocation=_transfer(event);
+//         streamController.add(_lastLocation!);
+//       }
+//     });
+//     return true;
+//   }
+//
+//
+//   XDLocation _transfer(LocationData locationData){
+//     return XDLocation(latitude:locationData.latitude!,longitude: locationData.longitude!,altitude: locationData.altitude );
+//   }
+//
+//   @override
+//   XDLocation? getLastPosition() {
+//     return _lastLocation;
+//   }
+// }
